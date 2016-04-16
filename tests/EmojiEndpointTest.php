805c62e06dc386fb44ac2726cd1d8c7be357703e
Raimi Ademola <ademola.raimi@andela.com>
2016-04-16 15:11:26 +0100

    rest everything

--- tests/EmojiEndpointTest.php
+++ tests/EmojiEndpointTest.php
@@ -199,6 +199,7 @@ class EmojiEndpointsTest extends PHPUnit_Framework_TestCase
 
     public function testCreateUser()
     {   
+        User::truncate();
         $env = Environment::mock([
             'REQUEST_METHOD' => 'POST',
             'REQUEST_URI'    => '/auth/register',
@@ -206,8 +207,8 @@ class EmojiEndpointsTest extends PHPUnit_Framework_TestCase
         ]);
 
         $body = [
-            'fullname' => 'prosper trainer',
-            'username' => 'trainer',
+            'fullname' => 'tester',
+            'username' => 'tester',
             'password' => 'test',
         ];
 
@@ -230,7 +231,7 @@ class EmojiEndpointsTest extends PHPUnit_Framework_TestCase
 
         $req = Request::createFromEnvironment($env);
         $req = $req->withParsedBody([
-            'username' => 'trainer',
+            'username' => 'tester',
             'password' => 'test',
         ]);
         $this->app->getContainer()['request'] = $req;
@@ -281,7 +282,7 @@ class EmojiEndpointsTest extends PHPUnit_Framework_TestCase
         $response = $this->app->run(true);
         $token = ( (string) $response->getBody());
        
-        $expect = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpYXQiOjE0NDAyOTU2NzMsImp0aSI6Ik1UUTBNREk1TlRZM013PT0iLCJuYmYiOjE0NDAyOTU2NzMsImV4cCI6MTQ0Mjg4NzY3MywiZGF0YSI6eyJ1c2VySWQiOm51bGx9fQ.Owt-mlXsl_JHmQVJq-QbF_2h3Cm9Pt3IzahOmuV93YE';
+        $expect = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpYXQiOjE0NDAyOTU2NzMsImp0aSI6Ik1UUTBNREk1TlRZM013PT0iLCJuYmYiOjE0NDAyOTU2NzMsImV4cCI6MTQ0Mjg4NzY3MywiZGF0YSI6eyJ1c2VySWQiOjF9fQ.iGP_Pwx2-WMDVodQcI5tcTWgh8LD21LwgQRFnH6IMmk';
         $this->assertEquals($expect, $token);
 
         $this->assertSame($response->getStatusCode(), 200);
