d2d9535f4afb5d8d715d6bee560b532d1d047875
Emmanuel Ade <emmade.tx@gmail.com>
2016-04-27 07:58:25 +0100

    remove try and catch

--- src/Middleware/AuthMiddleware.php
+++ src/Middleware/AuthMiddleware.php
@@ -6,7 +6,6 @@
  */
 namespace Demo;
 
-//use Exception;
 use Firebase\JWT\JWT;
 
 class AuthMiddleware
@@ -23,8 +22,7 @@ class AuthMiddleware
     public function __invoke($request, $response, $next)
     {
        $authHeader = $request->getHeader('HTTP_AUTHORIZATION');
-
-        try {
+        
            if (!empty($authHeader)) {
                 $appSecret    = getenv('APP_SECRET');
                 $jwt          = $authHeader[0];   
@@ -32,9 +30,6 @@ class AuthMiddleware
                 
                 return $next($request, $response);
             }
-        } catch (Exception $e) {
-            return $response->withJson(['status: Token invalid or Expired'], 500);
-        }
        
         return $response->withJson(['message' => 'User unauthorized due to empty token'], 401);
    }
